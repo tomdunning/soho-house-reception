cb802a01ba14e51983411eadfc31b1607d49674e
Tom Dunning <tom.dunning@ad-signal.io>
2019-06-26 16:52:26 +0100

    Locations - red

---
+++ spec/factories/locations.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :location do
+    sequence :name do |n|
+      "Location #{n}"
+    end
+
+    trait :with_address do
+      address_line_1 { "123 Fake Street" }
+      address_line_2 { "York" }
+      postcode { 'YO26 5TB' }
+      country_code { 'GBR' }
+    end
+  end
+end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -3,17 +3,40 @@ require 'rails_helper'
 RSpec.describe Location, type: :model do
 
   describe '#validate' do
+    subject { described_class.new(params) }
+
     context 'with required params' do
-      it 'is valid'
+      let(:params) { attributes_for(:location) }
+      it 'is valid' do
+        expect(subject).to be_valid
+      end
     end
 
     context 'with required and optional params' do
-      xit 'is valid'
+      let(:params) { attributes_for(:location, :with_address) }
+
+      it 'is valid' do
+        expect(subject).to be_valid
+      end
+    end
+
+    context 'without minimum params' do
+      let(:params) { {} }
+      it 'is invalid and returns errors' do
+        expect(subject).not_to be_valid
+        expect(subject.errors[:name]).to include("can't be blank")
+      end
     end
 
 
-    context 'with invalid params' do
-      xit 'is invalid and returns errors'
+    context 'with a duplicate name' do
+      let(:location_1) { create(:location) }
+      let(:params) { { name: location_1.name } }
+
+      it 'is invalid and returns errors' do
+        expect(subject).not_to be_valid
+        expecT(subject.errors[:name]).to include("has already been taken")
+      end
     end
   end
 end
